f8721d1a9417e3b69d1c46a2a2bed3d73784b605
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2023-08-19 19:33:09 +0530

    Update Student.java
    *Define a class Student having data members' name, roll no, and percentage. Accept for five objects and display the merit list containing merit  number, name, roll no, and percentage by using array of objects.

--- Student.java
+++ Student.java
@@ -31,3 +31,45 @@ public class Student {
 	  System.out.println("Merit No: "+(m+1)+" Name: "+ name +" Roll No: "+ roll_no + "Percent: "+ percent);
 	}
 }
+
+
+// main class Program4.java 
+
+package first;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		
+		Student s[] = new Student[5];
+		Student s1 = new Student();
+		
+		for(int i=0;i<5;i++) {
+			s[i] = new Student();
+		}
+		
+		for(int i=0;i<5;i++) {
+			s[i].get();
+		}
+		
+		double test  = s[0].percent;
+		int j=0;
+		
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+				
+				if(s[i].percent < s[j].percent) {
+					s1 = s[i];
+					s[i] = s[j];
+					s[j] = s1;
+				}
+			}
+		}
+		
+		for(int i=0;i<5;i++) {
+			s[i].show(i);
+		}
+	}
+
+}
+
