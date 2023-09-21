371b557fdc48aa997e09054a1cbc93ed551a7a85
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2023-09-21 17:32:09 +0530

    Create FileChooser1.java

---
+++ FileChooser1.java
@@ -0,0 +1,17 @@
+package first;
+import java.io.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.filechooser.*;
+
+public class FileChooser1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFileChooser jf = new JFileChooser();
+		jf.showSaveDialog(null);
+
+	}
+
+}
